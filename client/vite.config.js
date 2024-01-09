4a749bc9b11a80bc7f19a3b73d1f78554661aad0
jayakumar03 <jaimech61@gmail.com>
2024-01-09 10:34:57 +0530

    Updated vite.congig.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const config = defineConfig({
+ plugins: [react()],
+ server: {
+   cors: true,
+ },
 })
+
+export default config
